eda2520437ccea42177b6d9d19fafcddd750aae2
Murlidhar Pandey <murlidharpandey@gmail.com>
2022-05-16 21:15:51 +0530

    added for paymentservice

---
+++ Crypto/paymentservice/src/main/java/com/redhat/paymentservice/util/Constants.java
@@ -0,0 +1,15 @@
+package com.redhat.paymentservice.util;
+
+public class Constants {
+   
+
+	public static final String DATE_FORMAT = "ddMMyyHHmmss";
+	public static final String UH = "Crypto";
+	public static final String STATUS_NEW = "N";
+	public static final String STATUS_WORKINPROGRESS = "W";
+	public static final String STATUS_CLOSED = "W";
+	public static final String SUCCESS = "SUCCESS";
+	public static final String FAILED_TO_GENERATE_COMPLAINT = "Failed to generate complaint";
+
+
+}
